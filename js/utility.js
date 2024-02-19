e9dd56ec0d82edcbca4075f39d6364a70c6e4de1
Bithe <farzana.hossain5@gmail.com>
2024-02-19 17:28:30 +0600

    fully done

--- js/utility.js
+++ js/utility.js
@@ -1,23 +1,26 @@
+function showInnerText(id, value) {
+  document.getElementById(id).innerText = value;
+}
 
-function showInnerText(id, value){
+function couponApply(value) {
+  const couponForm = document.getElementById("coupon-from");
+  couponForm.classList.add("hidden");
 
-    document.getElementById(id).innerText = value;
-    
+  const discountInfo = document.getElementById("discount-info");
+  const li = document.createElement("li");
+  const discountShow = document.createElement("p");
+  discountShow.innerText = "Discount";
+  const discountAmount = document.createElement("p");
+  discountAmount.innerText = value;
+
+  discountInfo.append(li);
+  li.append(discountShow);
+  li.append(discountAmount);
+}
+
+// clear all after clicking continue
+function clearAll(){
+    location.reload();
 }
 
 
-function couponApply(value){
-    const couponForm = document.getElementById('coupon-from');
-    couponForm.classList.add("hidden");
-
-    const discountInfo = document.getElementById('discount-info');
-    const li = document.createElement("li");
-    const discountShow = document.createElement("p");
-    discountShow.innerText = "Discount";
-    const discountAmount = document.createElement("p");
-    discountAmount.innerText = value;
-
-    discountInfo.append(li);
-    li.append(discountShow);
-    li.append(discountAmount);
-}
